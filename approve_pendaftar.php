004b440ae952dc10703fab6a89cd56e5adebb3bc
Widiani23 <52641505+Widiani23@users.noreply.github.com>
2019-07-25 10:32:51 +0700

    Add files via upload

---
+++ approve_pendaftar.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="../assets/bootstrap/css/bootstrap.css">
+  <script type="text/javascript" src="../assets/js/jquery-2.1.1.js"></script>
+  <script type="text/javascript" src="../assets/bootstrap/js/bootstrap.js"></script>
+   <link rel="stylesheet" href="../assets/plugins/datatables/dataTables.bootstrap.css">
+</head>
+<body>
+<section class="content">
+
+            <div class="box">
+            <div class="box-header"> 
+            <a type="button"  class='btn btn-info'> <i class="fa fa-plus"></i> Tambah Baru</a>
+            </div>
+
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>No</th>
+                  <th>NIK</th>
+                  <th>Posisi</th>
+                  <th>Opsi</th>
+                  </tr>
+                </thead>
+    <tbody>
+            <!--  -->
+            <tr>
+            <td></td> 
+            <td></td>
+            <td></td>
+            <td></td>
+            </tr>
+            
+              </tbody>
+              </table>
+              </div>
+              </div>
+    </section>
+</body>
+</html>
